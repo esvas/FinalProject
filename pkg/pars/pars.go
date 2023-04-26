e5dc7be697dc1658109b7dc232f292bc12c28127
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:53:10 +0300

    Add files via upload

--- pkg/pars/pars.go
+++ pkg/pars/pars.go
@@ -1,1 +1,32 @@
+package pars
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func FileToStr(filePath string) ([]string, error) {
+	content, err := ReadFile(filePath)
+	listStrings := strings.Split(string(content), "\n")
+	return listStrings, err
+}
+
+func ReadFile(filePath string) ([]byte, error) {
+	log.Printf("Извлечение данных из файла `%v`", filePath)
+	content, err := os.ReadFile(filePath)
+	return content, err
+}
+
+func JSON[T any](storage *T, r io.Reader) error {
+	content, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(content, &storage); err != nil {
+		return err
+	}
+	return nil
+}
