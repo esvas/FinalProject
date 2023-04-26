967d24c56f732689bc6922dd36ee90892a016e52
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:51:24 +0300

    Add files via upload

--- internal/support/support.go
+++ internal/support/support.go
@@ -1,1 +1,10 @@
+package support
 
+type Support struct {
+	Topic         string `json:"topic"`
+	ActiveTickets int    `json:"active_tickets"`
+}
+
+func (s *Support) Check() bool {
+	return s.Topic != "" && s.ActiveTickets != 0
+}
