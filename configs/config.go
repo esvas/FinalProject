8a12fbd8fa7b9239d77b09fff74c6563c5781805
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:45:29 +0300

    Add files via upload

--- configs/config.go
+++ configs/config.go
@@ -1,1 +1,38 @@
+package configs
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	SMS       string `json:"SMS"`
+	VoiceCall string `json:"voice_call"`
+	Billing   string `json:"billing"`
+	MMS       string `json:"MMS"`
+	Support   string `json:"support"`
+	Incident  string `json:"incident"`
+	Email     string `json:"email"`
+}
+
+func GetConfig() Config {
+	dir, err := filepath.Abs("")
+	if err != nil {
+		log.Printf("Ошибка пути к файлу настроек:\n%v", err)
+	}
+	pathConfigFile := filepath.Join(dir, "configs", "config.json")
+	log.Println(pathConfigFile)
+	configByte, err := os.ReadFile(pathConfigFile)
+	if err != nil {
+		log.Printf("Ошибка получения настроек:\n%v", err)
+		os.Exit(1)
+	}
+	var config Config
+	if err = json.Unmarshal(configByte, &config); err != nil {
+		log.Printf("Ошибка получения настроек:\n%v", err)
+		os.Exit(1)
+	}
+	return config
+}
