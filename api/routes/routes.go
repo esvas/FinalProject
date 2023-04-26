cbee124303fa2e202d58590dd5cb277b2cc0310a
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:43:36 +0300

    Add files via upload

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,1 +1,18 @@
+package routes
 
+import (
+	. "github.com/esvas/FinalProject/api/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func CreateRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", ConnectionHandler)
+	})
+
+	return r
+}
