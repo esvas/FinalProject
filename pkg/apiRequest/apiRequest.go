d294242783cda70919fec52c06ae940c00a8cbc1
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:52:55 +0300

    Add files via upload

--- pkg/apiRequest/apiRequest.go
+++ pkg/apiRequest/apiRequest.go
@@ -1,1 +1,11 @@
+package apiRequest
 
+import "net/http"
+
+func Get(url string) *http.Response {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil
+	}
+	return resp
+}
