c586e32a894ffa0a61142256811b410e85cf5e15
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:43:02 +0300

    Add files via upload

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,1 +1,15 @@
+package handlers
 
+import (
+	"encoding/json"
+	"github.com/esvas/FinalProject/internal/storages"
+	"net/http"
+)
+
+func ConnectionHandler(w http.ResponseWriter, _ *http.Request) {
+	res := storages.GetResultData()
+	resBytes, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(resBytes)
+}
