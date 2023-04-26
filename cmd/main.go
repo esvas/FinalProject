c733679d16e90fde1cd3b4bee24839b78e7dbcf2
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:44:39 +0300

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/esvas/FinalProject/api/routes"
+)
+
+func main() {
+	log.Println("Запуск сервера")
+	r := routes.CreateRouter()
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Printf("Не удалось запустить сервер:\n%v", err)
+		os.Exit(1)
+	}
+}
