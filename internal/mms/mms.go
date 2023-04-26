449f9ce972cd16405296b5c1ed64e4b22ecf8b23
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:50:20 +0300

    Add files via upload

--- internal/mms/mms.go
+++ internal/mms/mms.go
@@ -1,1 +1,40 @@
+package mms
 
+import (
+	"github.com/ferdypruis/iso3166"
+	"golang.org/x/exp/slices"
+	"strconv"
+)
+
+var allowedProviders = []string{"Topolo", "Rond", "Kildy"}
+
+type MMS struct {
+	Country      string `json:"country"`
+	Provider     string `json:"provider"`
+	Bandwidth    int    `json:"bandwidth"`
+	ResponseTime int    `json:"response_time"`
+}
+
+func New(country, provider, bandwidthSTR, avgRespTime string) *MMS {
+	c, err := iso3166.FromAlpha2(country)
+	if err != nil {
+		return nil
+	}
+	bandwidth, err := strconv.Atoi(bandwidthSTR)
+	if err != nil || (bandwidth < 0 && bandwidth > 100) {
+		return nil
+	}
+	if !slices.Contains(allowedProviders, provider) {
+		return nil
+	}
+	ResponseTime, err := strconv.Atoi(avgRespTime)
+	if err != nil {
+		return nil
+	}
+	return &MMS{
+		Country:      c.Name(),
+		Bandwidth:    bandwidth,
+		ResponseTime: ResponseTime,
+		Provider:     provider,
+	}
+}
