cb9e5816b00dd7ce028bbc3481cf649703b55c41
esvas <127389605+esvas@users.noreply.github.com>
2023-04-26 21:50:01 +0300

    Add files via upload

--- internal/incident/incident.go
+++ internal/incident/incident.go
@@ -1,1 +1,17 @@
+package incident
 
+import "golang.org/x/exp/slices"
+
+type Incident struct {
+	Topic  string `json:"topic"`
+	Status string `json:"status"` // возможные статусы active и closed
+}
+
+func (i *Incident) Check() bool {
+	return i.Topic != "" && i.checkStatus()
+}
+
+func (i *Incident) checkStatus() bool {
+	allowedStatuses := []string{"closed", "active"}
+	return slices.Contains(allowedStatuses, i.Status)
+}
